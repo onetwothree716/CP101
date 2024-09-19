5027a7523c3cbd332b34e08d7d60770f786f4682
fernando subido <fernandosubido293@gmail.com>
2024-09-19 22:37:59 +0800

    Update Activity 5.py

--- Activity 5/Activity 5.py
+++ Activity 5/Activity 5.py
@@ -1,1 +1,2 @@
 Create a code that will count all lower case, upper case, digits, and special symbols from a given string.
+statement = "Hi My Name Is Fernando Subido My Passion Is Playing Basketball And My Position Is Shooting Guard!"
